ca3f39a50f720bfcd958fec02be956b8a50195ea
suryavenkat1824 <kesarapu20_ug@cse.nits.ac.in>
2024-07-21 16:59:13 +0530

    Port change for render error cors removal

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const vendorRoutes=require('./routes/vendorRoutes')
 const bodyParser=require('body-parser')
 const firmRoutes=require('./routes/firmRoutes')
 const productRoutes=require('./routes/productRoutes')
-const cors=require('cors')
+ 
 const path=require('path')
 const app=express()
 
 const PORT=process.env.PORT ||  4000 ;
 dotEnv.config();
-app.use(cors());
+ 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDb Connected Succesfully"))
 .catch((error)=>console.log(error))
