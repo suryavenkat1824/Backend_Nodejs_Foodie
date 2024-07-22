ec4d7e5d9e402b2fea8da66f8567182076c40b2a
suryavenkat1824 <kesarapu20_ug@cse.nits.ac.in>
2024-07-22 23:01:15 +0530

    path added

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
 const path = require('path')
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 4000;
 
--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -2,7 +2,7 @@ const express=require('express')
 
 const firmController=require('../controllers/firmController')
 const verifyToken= require('../middlewares/verifyToken')
-const router= express.Router()
+const router= express.Router();
 
 router.post('/add-firm', verifyToken, firmController.addFirm)
 router.get('/uploads/:imageName',(req,res)=>{
